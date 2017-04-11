8a442105ef8b0d51a3b2ac94d61d959fd4c66cb7
thash <hash@hzl.jp>
2017-04-11 20:29:02 +0900

    improve seed.rb script to skip same line and make sure update latest local item seq_ids

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 gem 'aws-sdk'
 gem 'pry'
+gem 'rb-readline'
 gem 'twurl' # https://dev.twitter.com/rest/reference/post/statuses/update
 gem 'unicode'
--- seed.rb
+++ seed.rb
@@ -2,6 +2,7 @@ require 'bundler'
 Bundler.require
 require 'pp'
 require 'pry'
+require 'rb-readline'
 
 table  = ENV['TABLE']
 dynamo = Aws::DynamoDB::Client.new(region: 'ap-northeast-1')
@@ -11,12 +12,12 @@ wcu    = dynamo.describe_table(table_name: table).table.provisioned_throughput.w
 items = open('./data.txt').read.split("\n")
                           .reject{|l| l =~ /^;;/ ? (puts l or true) : false }
 
-puts "total item count: #{items.count}"
-pp dynamo.update_item(table_name: table,
-                      key: { text_id: 0, seq_id: 0},
-                      update_expression: 'SET #c = :val',
-                      expression_attribute_names: { '#c' => 'count' },
-                      expression_attribute_values: { ':val' => items.count })
+puts "update total item count: #{items.count}"
+dynamo.update_item(table_name: table,
+                   key: { text_id: 0, seq_id: 0},
+                   update_expression: 'SET #c = :val',
+                   expression_attribute_names: { '#c' => 'count' },
+                   expression_attribute_values: { ':val' => items.count })
 
 def text_to_tweets(text, cursor)
   if text.length > cursor
@@ -27,7 +28,41 @@ def text_to_tweets(text, cursor)
   end
 end
 
+def query_target_text_items(dynamo, table, i)
+  dynamo.query(table_name: table,
+               key_condition_expression: "#text_id = :i",
+               expression_attribute_names: { "#text_id": "text_id" },
+               expression_attribute_values: { ":i": i }).items
+end
+
+def remote_line(target_text_items)
+  target_text_items[0]['chapter'] + ',' +
+    target_text_items[0]['page'] + ',' +
+    target_text_items.map{|item| item['text'] }.join
+end
+
 items.each.with_index do |line, i|
+  i += 1
+  #puts "item No. #{i}..."
+
+  remote_items = query_target_text_items(dynamo, table, i)
+  unless remote_items.empty?
+    remote_md5 = Digest::MD5.hexdigest(remote_line(remote_items))
+    local_md5  = Digest::MD5.hexdigest(line)
+    next if local_md5 == remote_md5
+    puts '---'
+
+    puts "local line: #{line}"
+    puts "delete remote items: #{remote_line(remote_items)}"
+
+    remote_items.each do |item|
+      pp({"text_id": {"N": i.to_s },
+          "seq_id":  {"N": item['seq_id'].to_i.to_s }})
+      dynamo.delete_item(table_name: table,
+                         key: {"text_id": i, "seq_id": item['seq_id'].to_i})
+    end
+  end
+
   chapter, page, text = line.split(',')
 
   puts text
@@ -37,7 +72,7 @@ items.each.with_index do |line, i|
 
   tweets.each.with_index do |t, seq_index|
     dynamo.put_item(table_name: table,
-                    item: {text_id: i + 1, seq_id: seq_index,
+                    item: {text_id: i, seq_id: seq_index,
                            chapter: chapter, page: page, text: t})
   end
   sleep 1 if (i % wcu).zero?
